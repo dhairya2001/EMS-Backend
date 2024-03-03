2c26c4e389492e0e9ac6ff3c2606ad536f2e1d88
dhairya2001 <dhairyachhabra3072@gmail.com>
2024-03-03 16:35:27 +0530

    cors removed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors=require("cors");
 const categoryRoute = require('./routes/categoryRoute');
 
 const app=express();
-app.use(cors()); 
+// app.use(cors()); 
 
 app.use(express.urlencoded({ extended: true }));
 
