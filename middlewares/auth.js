185aeb0306dc641413bc61f15a7ce2a0caf94454
dhairya2001 <dhairyachhabra3072@gmail.com>
2024-03-09 18:42:26 +0530

    debug middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt=require("jsonwebtoken");
-
+const SECERET_KEY="EMSAPI";
 const auth = (req,res,next) =>{
     try {
         let token=req.headers.authorization;
+        console.log(token);
         if(token){
             token=token.split(" ")[1];
-            let user=jwt.verify(token,process.env.SECERET_KEY);
+            let user=jwt.verify(token,SECERET_KEY);
             req.userId=user.id;
         }
         else{
