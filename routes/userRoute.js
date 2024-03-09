ba81c3820c0c7787d3ef27c70de5733a8ad508c3
dhairya2001 <dhairyachhabra3072@gmail.com>
2024-03-09 15:14:44 +0530

    added middleware in userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const auth=require('../middlewares/auth.js');
 const { signUp, logIn } = require('../controller/userController.js');
 const userRoute=express.Router();
 
 userRoute.post('/signUp',signUp);
 userRoute.post('/logIn',logIn);
+userRoute.use(auth); // Use the auth middleware for all routes defined in userRoute
 module.exports=userRoute;
