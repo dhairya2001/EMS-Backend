373bc62658a7b2b3a5b8dc6b4cf2f24b1dbee8f2
dhairya2001 <dhairyachhabra3072@gmail.com>
2024-03-09 19:34:34 +0530

    changed middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,19 @@
-const jwt=require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const auth = (req,res,next) =>{
-    try {
-        let token=req.headers.authorization;
-        if(token){
-            console.log(token);
-            token=token.split(" ")[1];
-            let user=jwt.verify(token,process.env.SECERET_KEY);
-            console.log(user);
-            req.userId=user.id;
-        }
-        else{
-            throw new Error("Authorization token not provided");
-        }
-    } catch(error) {
-        console.log("Token validation failed");
-        res.status(401).json({message:"Unauthorized User!"})
-    } 
+const auth = (req, res, next) => {
+  let token = req.header('Authorization');
+  token=token.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Authorization token is required.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "EMSAPI");
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token.' });
+  }
 };
 
 module.exports = auth;
